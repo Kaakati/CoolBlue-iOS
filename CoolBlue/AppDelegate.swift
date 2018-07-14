6bc1601f36a01e781856dba18085fa9fb065545a
Mohamad Kaakati <m@kaakati.me>
2018-07-14 16:48:09 +0300

    Setting App Project.

--- CoolBlue/AppDelegate.swift
+++ CoolBlue/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Set Key Window and Screen
+        window = UIWindow()
+        self.window?.backgroundColor = UIColor.white
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
